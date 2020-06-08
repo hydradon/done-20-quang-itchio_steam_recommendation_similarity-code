0a872f12d0430bd385b7776bccc871c620468298
Quang <4397332+hydradon@users.noreply.github.com>
2020-06-07 19:31:29 -0600

    Add sql to build database

---
+++ scripts/sql/create_db.sql
@@ -0,0 +1,45 @@
+CREATE DATABASE IF NOT EXISTS gamerecs 
+CHARACTER SET = utf8mb4 
+COLLATE = utf8mb4_unicode_ci;
+
+USE gamerecs;
+
+CREATE TABLE IF NOT EXISTS `toprecs`
+(
+	`itch_game` VARCHAR(255),
+    `itch_game_url` VARCHAR(255),
+    `steam_game` VARCHAR(255),
+	`steam_game_index` INT,
+    `steam_game_url` VARCHAR(255),
+    `sim_scores` FLOAT,
+    `game_desc_snippet` TEXT,
+    `game_no_ratings` INT,
+    `row_id` INT
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+
+CREATE TABLE IF NOT EXISTS `rec_ratings`
+(
+	`itch_game_url` VARCHAR(255),
+    `steam_game_url` VARCHAR(255),
+    `is_upvote` BOOLEAN,
+    `feedback` TEXT,
+    `u_id` INT,
+    `timestamp` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+
+CREATE TABLE IF NOT EXISTS `dev_twitter_uid`
+(
+	`main_twitter_link` VARCHAR(255),
+    `uid` INT
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+
+CREATE TABLE IF NOT EXISTS `game_twitter_devs`
+(
+	`game_name` VARCHAR(255),
+	`game_url` VARCHAR(255),
+    `main_twitter_link` VARCHAR(255),
+    `uid` INT
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
---
+++ scripts/sql/export_rating_to_csv.sql
@@ -0,0 +1,34 @@
+USE gamerecs;
+
+SELECT itch_game_url, steam_game_url, is_upvote, feedback, u_id, timestamp, sim_scores
+FROM (
+	(SELECT 'itch_game_url' as itch_game_url, 
+			'steam_game_url' as steam_game_url, 
+			'is_upvote' as is_upvote, 
+			'feedback' as feedback, 
+			'u_id' as u_id, 
+			'timestamp' as timestamp, 
+			'sim_scores' as sim_scores,
+			1 as which)
+	UNION ALL
+	(SELECT rating.itch_game_url, 
+			rating.steam_game_url,
+			cast(rating.is_upvote AS CHAR),
+			rating.feedback,
+			cast(rating.u_id AS CHAR),
+			cast(rating.timestamp AS CHAR),
+			cast(rec.sim_scores AS CHAR),
+			2 as which
+	FROM rec_ratings rating JOIN toprecs rec
+	ON rating.itch_game_url = rec.itch_game_url
+	AND rating.steam_game_url = rec.steam_game_url)
+) result
+ORDER BY which, -- header comes first
+		 result.itch_game_url ASC, 
+         result.sim_scores + 0.0 DESC, 
+         result.u_id ASC, 
+         result.timestamp ASC,
+         result.is_upvote DESC -- Should order by is_upvote?
+INTO OUTFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/ratings_result.csv'
+FIELDS ENCLOSED BY '"' TERMINATED BY ',' ESCAPED BY '"'
+LINES TERMINATED BY '\r\n';
